c26882f5d470510011f6ee827519432be40ab1a1
Manoel-I <manoel.122112@outlook.com>
2023-07-07 17:18:46 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,95 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h3> Cadastro: </h3>
+    <small id="errorName" v-show="erroNome" >nome vazio ou invalido</small><br>
+    <input type="text" placeholder="nome" v-model="nomeFild"><br>
+    <input type="email" placeholder="email" v-model="emailFild"><br>
+    <input type="number" placeholder="idade" v-model="idadeFild"><br>
+    <button @click="cadastrarUsuario()" >Cadastrar</button>
+
+
+    <div v-for="(cliente, index) in orderClientes " :key="cliente.id">
+      <h4>{{index + 1}}</h4>  
+      <Cliente_comp :cliente="cliente" @meDelete="deleteUsuario($event)"/> <!--@meDelete = chamando / "deleteUsuario" = escutando  -->
+    </div>                                                                  <!-- o parametro $event recebe o que é enviado pelo $emit -->
+  </div> 
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//importação do componente
+import Cliente_comp from "./components/Cliente_comp";
+import _ from "lodash";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      erroNome : false,
+      nomeFild : '',
+      emailFild : '',
+      idadeFild : 0,
+
+      clientes: [
+        {
+          id : 1,
+          nome : "manoel",
+          email : "manoel@gmail.com",
+          idade : 24
+        },
+        {
+          id : 2,
+          nome : "marcos",
+          email : "marcos@gmail.com",
+          idade : 89
+        },
+        {
+          id : 3,
+          nome : "mauricio",
+          email : "mauricio@gmail.com",
+          idade : 67
+        }
+      ]
+    }
+  },
+  components:{
+    Cliente_comp,
+  },
+  methods:{
+    cadastrarUsuario: function(){
+      if(this.nomeFild == '' || this.nomeFild == ' ' || this.nomeFild.length < 2){
+        console.log(this.erroNome);
+        this.erroNome = true;
+      }else{
+        this.clientes.push({
+          nome : this.nomeFild,
+          email : this.emailFild,
+          idade : this.idadeFild,
+          id :  Date.now()
+        });
+      this.nomeFild = "";
+      this.emailFild = "";
+      this.idadeFild = 0;
+      this.erroNome = false;
+      } 
+    },
+    deleteUsuario: function($event){ //o $event é o que é recebido atraves do metodo no componente filho
+      let id = $event.idCliente;
+      let novoArray = this.clientes.filter(cliente => cliente.id != id);
+      this.clientes = novoArray;
+    }
+  },
+  computed:{
+    orderClientes: function(){
+        return _.orderBy(this.clientes, ['nome'], ['asc']);
+    }
   }
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#errorName{
+  color : red;
 }
+
 </style>
---
+++ src/components/Cliente_comp.vue
@@ -0,0 +1,64 @@
+<template>
+    <div :class="{'cliente': !isPremium, 'cliente-premium' : isPremium}">
+        <h4>Nome : {{ uppercaseFilter}}</h4>
+        <hr>
+        <p>Email : {{cliente.email}}</p>
+        <p>Idade : {{cliente.idade}}</p>
+        <p>ID : {{cliente.id}}</p>
+        <button @click="mudarCor($event)">Mudar Cor</button>
+        <button @click="meDelete()">Deletar</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            isPremium : false
+        }
+    }, 
+    props: {
+        cliente : Object,
+    },
+    methods:{
+        mudarCor: function($event){
+            console.log($event);
+            this.isPremium = !this.isPremium;
+        }, 
+        meDelete: function(){
+            console.log(this);
+            this.$emit("meDelete",{idCliente : this.cliente.id}); // $emit = emitir / então aqui estamos emitindo o metodo "meDelete"
+        }
+    },
+    computed: { // filters foram descontinuados, agora o cumputed que tem a mesma função - / eles não mudam o valor só a exibição
+        uppercaseFilter(){
+            console.log( this.cliente.nome);
+            return this.cliente.nome.toUpperCase();
+        }
+    }
+}
+
+
+</script>
+
+
+<style>
+.cliente{
+    background-color : #ECE5E3;
+    max-width : 600px;
+    height : 180px;
+    padding : 1%;
+    margin-top : 2%;
+}
+
+.cliente-premium{
+    background-color : black;
+    color : yellow;
+    max-width : 600px;
+    height : 180px;
+    padding : 1%;
+    margin-top : 2%;
+
+}
+
+</style>
